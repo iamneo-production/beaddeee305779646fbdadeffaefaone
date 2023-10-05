6f12190a2746fc0221db60e85fed12da9258dc2b
Iamneo <techadmin@iamneo.ai>
2023-10-05 07:21:21 +0000

    Backup Commit

--- dotnetproject/dotnetapiapp/Data/FreelancerProjectDbContext.cs
+++ dotnetproject/dotnetapiapp/Data/FreelancerProjectDbContext.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotnetapiapp.Data
 {
-    public class FreelancerProjectDbContext
+    public class FreelancerProjectDbContext : DbContext
     {
-        
+        public FreelancerProjectDbContext(DbContextOptions<FreelancerProjectDbContext> options)
+        : base(options)
+        {
+        }
     }
 }
--- dotnetproject/dotnetapiapp/Models/Project.cs
+++ dotnetproject/dotnetapiapp/Models/Project.cs
@@ -2,11 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace dotnetapiapp.Models
 {
     public class Project
     {
-        
+         [key]
+        public int ProjectID {get; set;}
+        [Required]
+        public int ProjectName {get; set;}
+        public int NumberofModules {get; set;}
+        public DateTime SubmissionDate {get; set;}
+        [ForeignKey("Freelancer")]
+        public int FreelancerID {get; set;}
+        public Freelancer Freelancer {get; set; }
     }
 }

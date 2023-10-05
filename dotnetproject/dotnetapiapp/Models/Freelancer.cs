906001b083db59c3ad44cbc49c10fea369fe8749
Iamneo <techadmin@iamneo.ai>
2023-10-05 07:16:21 +0000

    Backup Commit

--- dotnetproject/dotnetapiapp/Models/Freelancer.cs
+++ dotnetproject/dotnetapiapp/Models/Freelancer.cs
@@ -2,11 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace dotnetapiapp.Models
 {
     public class Freelancer
     {
-        
+        [key]
+        public int FreelancerID {get; set;}
+        [Required]
+        public int FreelancerName {get; set;}
+        [Required]
+        public int Specialization {get; set;}
+        [Column(TypeName = "decimal(18,2)")]
+        public int CommercialPerHour {get; set;}
+        [Required]
+        public int MailID {get; set;}
+        public int ContactNumber {get; set;}
+        public ICollection<Project> projects {get; set; }
     }
 }
